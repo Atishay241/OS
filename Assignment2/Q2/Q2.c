51583317d2dbf563d6fd9b99c1f06e95d65ffa39
Atishay21241 <atishay21241@iiitd.ac.in>
2022-11-24 03:12:25 +0530

    Change

--- Assignment2/Q2/Q2.c
+++ Assignment2/Q2/Q2.c
@@ -10,8 +10,23 @@ int main(int argc, char **argv)
 	float matrix1[2][2] = {{5.1,2.1},{3.7,4.99}};
 	float matrix2[2][2];
 
-	printf("Making system call with matrix1 and matrix2\n");
+	printf("\nInitial Matrix1: \n");
+	for(int i=0;i<2;i++){
+		for(int j=0;j<2;j++){
+			printf("%f ",matrix1[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nInitial Matrix2: \n");
+	for(int i=0;i<2;i++){
+		for(int j=0;j<2;j++){
+			printf("%f ",matrix2[i][j]);
+		}
+		printf("\n");
+}
+
 	
+	printf("\n\nMaking system call with matrix1 and matrix2\n");
 	long res =  syscall(SYS_kernel_2d_memcpy, (float *)matrix1 , (float *)matrix2);
 	
 	printf("System call returned %ld.\n",res);
